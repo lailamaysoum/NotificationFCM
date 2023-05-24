023d993b29c6d8e326cea6c6f8f8ad1c1ee9a3fd
lailamaysoum <lailaelsous1@gmail.com>
2023-05-24 17:19:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notificationfcm/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.notificationfcm;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etToken;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etToken = findViewById(R.id.etToken);
+
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("TAG", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+
+                        Log.d("TAG",token);
+                        Toast.makeText(MainActivity.this, "your device token is : " + token, Toast.LENGTH_SHORT).show();
+                        etToken.setText(token);
+
+                    }
+                });
+    }
+}
